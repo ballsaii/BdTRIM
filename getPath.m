1a705e4de563c1a7542441b75e4529c7f35119aa
Chaipattana <g.ballsaii@gmail.com>
2019-11-15 11:43:10 +0700

    - supported MultiSelect

--- getPath.m
+++ getPath.m
@@ -2,7 +2,9 @@ function [filepath] = getPath
 % library that return path as object
 
 % uigetfile for single file (simutanously)
-[file,path,filter_index]=uigetfile('*.txt','Select TRIM distribution file','BdTRIM:Select');
+% support multi-selection
+[file,path,filter_index]=uigetfile('*.txt','Select TRIM distribution file','BdTRIM:Select'...
+    ,'MultiSelect','on');
 filepath = fullfile(path,file);
 % uigetfile from loading
 
--- readTRIM.m
+++ readTRIM.m
@@ -1,8 +1,19 @@
 function data = readTRIM()
-% 
+% create data structure
+data = struct('data',[],'path',[]);
+
 % call library that get path(s)
 path=getPath;
-data=importTRIM(path);
 
+% support multi file
+if iscell(path) % multi selction case
+    for i=1:length(path)
+        data(i).data=importTRIM(path{i});
+        data(i).path=path{i};
+    end
+else
+    data(1)=importTRIM(path);% single
+    data(1).path=path;
+end
 end
 
