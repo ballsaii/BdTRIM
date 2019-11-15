6c973cc5c825e821771fc8cee23299519b492a26
Chaipattana <g.ballsaii@gmail.com>
2019-11-15 11:09:06 +0700

    - working native listbox

--- new_workspacefig.m
+++ new_workspacefig.m
@@ -13,15 +13,5 @@
     dummyHandles(current_no_workspace).result_fig.Name = num2str(current_no_workspace);
     
     % 
-    jList = java.util.ArrayList;  % any java.util.List will be ok
-%     jList.add(0,'First');
-%     jList.add(1,'Second');
-%     jList.add(2,'Third');
-%     jList.add(3,'and last');
-    jCBList = com.mathworks.mwswing.checkboxlist.CheckBoxList(jList);
-    jScrollPane = com.mathworks.mwswing.MJScrollPane(jCBList);
-    [jhCBList,hContainer] = javacomponent(jScrollPane,[10,10,80,65],gcf);
-    set(jCBList, 'ValueChangedCallback', @myMatlabCallbackFcn);
-
     
     end
