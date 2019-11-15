c32c50d3cfc8fb83041433f4e5e147a645df7a50
Chaipattana <g.ballsaii@gmail.com>
2019-11-15 10:57:22 +0700

    - call external 'new_workspace' function for open new workspace

--- main.m
+++ main.m
@@ -1,28 +1,26 @@
 function [outputArg1,outputArg2] = main()
 %   This main function calls the 'main.fig'
 
-%   Initialize the main figure
+%   Initialize the main figure main_fig is an object-handle while Mhandles is a
+%   handle (data)
     main_fig = open('main.fig');
     Mhandles = guihandles(main_fig);
     guidata(main_fig,Mhandles)
     
 %   Initialized call_back function to objects
-    set(Mhandles.menu_new_resultfig,'Callback',{@new_resultfig});
+    set(Mhandles.menu_new_workspace,'Callback',{@new_workspace});
     
     % Intialized #object for result fig
-    no_window_result=1;
+    current_no_workspace=1;
     
-    function new_resultfig(hObject,evendata)
-%   Initialize a new result fig
-    result_fig = open('result.fig');
-    Rhandles = guihandles(result_fig);
-    guidata(result_fig,Rhandles);
-
-    % Create a dummy GUI handle
-    gui1Handles(no_window_result) = guidata(result_fig);
-    % Load data to result window
-    gui1Handles(no_window_result).result_fig.Name = num2str(no_window_result);
-    no_window_result = no_window_result+1;
+    function new_workspace(hObject,evendata)
+    
+    % call function for open resultfig
+    new_workspacefig(current_no_workspace)
+    
+    % increase index for next workspace
+    current_no_workspace = current_no_workspace+1;
+    
     end
     
 end
