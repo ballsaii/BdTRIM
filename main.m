0253de21dc61a9411af2477c729b4723a5bc996f
Chaipattana <g.ballsaii@gmail.com>
2019-11-18 13:25:55 +0700

    - prepare Openfile
    try to use internal function

--- main.m
+++ main.m
@@ -7,30 +7,22 @@ function [outputArg1,outputArg2] = main()
     Mhandles = guihandles(main_fig);
     guidata(main_fig,Mhandles)
     
-    % set window arragnement
-    screensize = get(0,'screensize');
-    screen_width = screensize(3);
-    screen_height = screensize(4);
-    
-    Mhandles.main_fig.OuterPosition=[0,0,screen_width/2,screen_height];
-    
 %   Initialized call_back function to objects
-    set(Mhandles.menu_new_workspace,'Callback',{@new_workspace});
+    Mhandles.btn_workspace.Callback = {@new_workspace};
     
     % Intialized #object for result fig
-    current_no_workspace=0;
+    current_no_workspace =0;
     
-    
-    
-    function new_workspace(hObject,evendata)
+    function new_workspace(hObject,evendata,handles)
     % increase index for next workspace
     current_no_workspace = current_no_workspace+1;  
     
     % call function for open resultfig
-    [res_fig,res_handles] = new_workspacefig(current_no_workspace);
-    guidata(Mhandles,res_fig)
+    fig=new_workspacefig(current_no_workspace);
+    hfig = guihandles(fig);
+    handles.workspacefig = hfig;
+    guidata(hObject,handles);
     end
 
-    
 end
 
--- new_workspacefig.m
+++ new_workspacefig.m
@@ -1,19 +1,12 @@
-    function [return_fig,return_handles] = new_workspacefig(current_no_workspace)
+function result_fig = new_workspacefig(current_no_workspace)
 %   Initialize a new result figure theme
-    result_fig = open('result.fig');
-    
+    result_fig = open('load.fig');
+%     data = guidata(result_fig);
+
     % Create a dummy GUI handle
     dummyHandles(current_no_workspace) = guihandles(result_fig);
     guidata(result_fig,dummyHandles(current_no_workspace));
     
-    % Update
-    guidata(result_fig,dummyHandles(current_no_workspace));
-
     % Load data to result window
-    dummyHandles(current_no_workspace).result_fig.Name = num2str(current_no_workspace);
-    
-    % return object and handles
-    return_fig = result_fig;
-    return_handles= dummyHandles(current_no_workspace);
-    
+    result_fig.Name = num2str(current_no_workspace);
     end
