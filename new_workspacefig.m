271fcf74e069a829b779ca214b096c0a68aca320
Chaipattana <g.ballsaii@gmail.com>
2019-11-18 15:58:02 +0700

    - Openfile and load in listbox
    - add files into the listbox

--- new_workspacefig.m
+++ new_workspacefig.m
@@ -1,12 +1,23 @@
 function result_fig = new_workspacefig(current_no_workspace)
 %   Initialize a new result figure theme
-    result_fig = open('load.fig');
+result_fig = open('load.fig');
 %     data = guidata(result_fig);
 
-    % Create a dummy GUI handle
-    dummyHandles(current_no_workspace) = guihandles(result_fig);
-    guidata(result_fig,dummyHandles(current_no_workspace));
-    
-    % Load data to result window
-    result_fig.Name = num2str(current_no_workspace);
+% Create a dummy GUI handle
+dummyHandles(current_no_workspace) = guihandles(result_fig);
+guidata(result_fig,dummyHandles(current_no_workspace));
+
+dummyHandles(current_no_workspace).btn_openfile.Callback = {@openfile};
+
+% Load data to result window
+result_fig.Name = sprintf('Workspace no %d',current_no_workspace);
+
+    function openfile(hObject,evendata,handles)
+        alldata = readTRIM;
+        for i=1:length(alldata)
+            list{i} = alldata(i).path;
+        end
+        old = get(dummyHandles(current_no_workspace).lst_load,'String');
+        dummyHandles(current_no_workspace).lst_load.String = [old',list];
     end
+end
---
+++ openfiles.m
@@ -0,0 +1,8 @@
+function  [path,data]=openfiles(hObject,evendata,handles)
+alldata = readTRIM;
+length(alldata)
+
+alldata(i).path
+
+end
+
